7eecfd97f9a7350a9070321d291732d76e87b71e
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2021-08-08 14:24:31 +0400

    db integrated

---
+++ server/migrations/20210808085218-category_add_field_desc_thumbnail.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn(
+        'categories',
+        'thumbnail',
+        Sequelize.STRING),
+
+      queryInterface.addColumn(
+        'categories',
+        'description',
+        Sequelize.TEXT
+      )
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn(
+        'categories',
+        'thumbnail',
+        Sequelize.STRING),
+
+      queryInterface.removeColumn(
+        'categories',
+        'description',
+        Sequelize.TEXT
+      )
+    ]);
+  }
+};
