90c1c229143dee303eb342812d73f66137fcd0bb
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2021-08-08 15:01:09 +0400

    category seed added

---
+++ server/seeders/20210808103350-add_default_categories.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return Promise.all([
+     queryInterface.bulkInsert('categories', [{
+       'title':'love diary',
+       'description':'All latest love sayari',
+       'createdAt': new Date(),
+       'updatedAt': new Date()
+     }], {})
+   ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('categories', null, {});
+  }
+};
